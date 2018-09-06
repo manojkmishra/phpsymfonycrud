4490bd8fbd74018bb434383cdf6cbe79bd054880
manojkmishra <manojmishra571980@gmail.com>
2018-09-06 19:10:56 +1000

    add post working

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Post;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 
 class PostController extends Controller
 {
@@ -23,8 +28,35 @@ class PostController extends Controller
      * @Route("/create", name="create_post_route")
      */
     public function createPostsAction(Request $request)
-    {
-       return $this->render('pages/create.html.twig');
+    {  $post= new Post;
+        $form=$this->createFormBuilder($post)
+        ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control')))
+        ->add('category', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('save', SubmitType::class, array('label' =>'Create Post', 'attr'=>array('class' => 'btn-btn-primary')))
+        ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $title=$form['title']->getData();
+            $description=$form['description']->getData();
+            $category=$form['category']->getData();
+          
+            $post->setTitle($title);
+            $post->setDescription($description);
+            $post->setCategory($category);
+
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            $this->addFlash('message','Post Created');
+            return $this->redirectToRoute('view_posts_route');
+
+
+        }
+
+       return $this->render('pages/create.html.twig', ['form'=>$form->createView()]);
     }
      /**
      * @Route("/view/{id}", name="view_post_route")
